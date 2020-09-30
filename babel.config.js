7e2af2ea2d944ff1767911cc84025245867c9c3d
Jose Pernia <josepernia91@gmail.com>
2020-09-30 17:07:52 -0400

    Adding module resolver for paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "~assets":  "./src/assets",
+            "~constants": "./src/constants",
+            "~components": "./src/components",
+            "~hooks": "./src/hooks",
+            "~navigation": "./src/navigation",
+            "~screens": "./src/screens",
+            "~types": "./src/types",
+          }
+        }
+      ]
+    ],
   };
 };
--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -3,11 +3,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+import Colors from '~constants/Colors';
+import useColorScheme from '~hooks/useColorScheme';
+import TabOneScreen from '~screens/TabOneScreen';
+import TabTwoScreen from '~screens/TabTwoScreen';
+import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '~types';
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
